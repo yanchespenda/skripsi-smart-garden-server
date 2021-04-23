e79c376017f4244668bab84f2bda1d3589111a8b
Alfian Rikzandi <denydirgantara94@gmail.com>
2021-04-23 13:04:32 +0700

    230421

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+echo "Kill all the running PM2 actions"
+sudo pm2 kill
+
+echo "Jump to app folder"
+cd /home/project
+
+echo "Update app from Git"
+git pull origin main
+
+echo "Install app dependencies"
+sudo rm -rf node_modules
+sudo yarn install
+
+echo "Build your app"
+sudo yarn build
+
+echo "Run new PM2 action"
+# sudo cp /home/project/ecosystem.json ecosystem.json
+sudo pm2 start ecosystem.json
